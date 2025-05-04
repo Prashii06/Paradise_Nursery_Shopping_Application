0fd3a1657f0902f73dea6217eae2acee3f7a3076
Prashii06 <prashii2806@gmail.com>
2025-05-04 13:03:10 +0530

    all done

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
-import './index.css';
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <HashRouter basename="/shoppingreact">
-      <App />
-    </HashRouter>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
